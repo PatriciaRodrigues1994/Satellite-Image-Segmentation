fcade6e3a6be03466a56e04bc346d133cbb80e2d
PatriciaRodrigues1994 <pats0994@gmail.com>
2020-01-15 17:38:52 +0530

    changes

---
+++ dataset_utils/dataset.py
@@ -0,0 +1,110 @@
+import numpy as np
+import torch.utils.data as data
+from PIL import Image
+import os
+import dataset_utils.transform_and_augment as transformer
+from pycocotools.coco import COCO
+from pycocotools import mask as cocomask
+import skimage.io as io
+import matplotlib.pyplot as plt
+import glob
+
+
+# Reference: https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py#L66
+class TrainImageDataset(data.Dataset):
+    def __init__(self, img_dir = None, cocodataset = None, y_data=None, input_img_resize=(128, 128), output_img_resize=(128, 128), X_transform=None, y_transform=None, threshold=0.5):
+        """
+            A dataset loader taking images paths as argument and return
+            as them as tensors from getitem()
+            Args:
+                threshold (float): The threshold used to consider the mask present or not
+                X_data (list): List of paths to the training images
+                y_data (list, optional): List of paths to the target images
+                input_img_resize (tuple): Tuple containing the new size of the input images
+                output_img_resize (tuple): Tuple containing the new size of the output images
+                X_transform (callable, optional): A function/transform that takes in 2 numpy arrays.
+                    Assumes X_data and y_data are not None.
+                    (train_img, mask_img) and returns a transformed version with the same signature
+                y_transform (callable, optional): A function/transform that takes in 2 numpy arrays.
+                    Assumes X_data and y_data are not None.
+                    (train_img, mask_img) and returns a transformed version with the same signature
+        """
+        self.threshold = threshold
+        self.y_train_masks = y_data
+        self.input_img_resize = input_img_resize
+        self.output_img_resize = output_img_resize
+        self.y_transform = y_transform
+        self.X_transform = X_transform
+        self.coco = cocodataset
+        self.X_train = self.coco.getImgIds(catIds=self.coco.getCatIds())
+        self.IMAGES_DIRECTORY = img_dir
+        
+        
+    def get_mask(self, img_index):
+        annotation_ids = self.coco.getAnnIds(imgIds=[img_index])
+        annotations = self.coco.loadAnns(annotation_ids)
+        mask = self.coco.annToMask(annotations[0])
+        for i in range(len(annotations)):
+            mask = mask | self.coco.annToMask(annotations[i])
+        return mask
+    
+    def __getitem__(self, index):
+        """
+            Args:
+                index (int): Index
+            Returns:
+                tuple: (image, target) where target is class_index of the target class.
+        """
+        index  = self.X_train[index]
+        img = self.coco.loadImgs(index)[0]
+        image_path = os.path.join(self.IMAGES_DIRECTORY, img["file_name"])
+        img = Image.open(image_path)
+        img = img.resize(self.input_img_resize, Image.ANTIALIAS)
+        img = np.asarray(img.convert("RGB"), dtype=np.float32)
+        # Pillow reads gifs
+        mask = self.get_mask(index)
+
+        if self.X_transform:
+            img, mask = self.X_transform(img, mask)
+
+        if self.y_transform:
+            img, mask = self.y_transform(img, mask)
+        
+        img = transformer.image_to_tensor(img)
+        mask = transformer.mask_to_tensor(mask, self.threshold)
+        return img, mask
+
+    def __len__(self):
+        return len(self.X_train)
+
+
+class TestImageDataset(data.Dataset):
+    def __init__(self, img_dir = None, img_resize=(128, 128)):
+        """
+            A dataset loader taking images paths as argument and return
+            as them as tensors from getitem()
+            Args:
+                X_data (list): List of paths to the training images
+                img_resize (tuple): Tuple containing the new size of the images
+        """
+        self.img_resize = img_resize
+        
+        self.X_train = glob.glob(img_dir + "/*.jpg")
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        img_path = self.X_train[index]
+        img = Image.open(img_path)
+        img = img.resize(self.img_resize, Image.ANTIALIAS)
+        img = np.asarray(img.convert("RGB"), dtype=np.float32)
+
+        img = transformer.image_to_tensor(img)
+        return img, img_path.split("/")[-1]
+
+    def __len__(self):
+        return len(self.X_train)
---
+++ src/main.py
@@ -0,0 +1,105 @@
+# import libraries
+from pycocotools.coco import COCO
+from pycocotools import mask as cocomask
+from multiprocessing import cpu_count
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+import random
+import argparse
+import os
+# helper functions/Classes for dataset preprocessing
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import RandomSampler, SequentialSampler
+import dataset_utils.transform_and_augment as trans_aug
+from dataset_utils.dataset import TrainImageDataset, TestImageDataset
+# additional functions/Classes for logging
+from model_utils.train_callbacks import TensorboardVisualizerCallback, TensorboardLoggerCallback, ModelSaverCallback
+from model_utils.test_callbacks import PredictionsSaverCallback
+from model_utils import helpers
+# additional functions/Classes for PyTorch Models
+from model_utils import classifier
+from model_utils import unet
+import torch.optim as optim
+
+
+
+def create_call_backs(args):
+	# Training callbacks
+	tb_viz_cb = TensorboardVisualizerCallback(os.path.join(args.project_dir, '/sum_logs/tb_viz'))
+	tb_logs_cb = TensorboardLoggerCallback(os.path.join(args.project_dir, '/sum_logs/tb_logs'))
+	model_saver_cb = ModelSaverCallback(os.path.join(args.project_dir,'/sum_logs/tb_logs/model_' +
+	                                                 helpers.get_model_timestamp()), verbose=True)
+	origin_img_size = 300
+	pred_saver_cb = PredictionsSaverCallback(os.path.join(args.project_dir, 'data/output/submit.csv.gz'),
+	                                             origin_img_size, args.threshold)
+
+	return tb_viz_cb, tb_logs_cb, model_saver_cb, pred_saver_cb
+
+def create_train_val_test_dataloaders(args, threads, use_cuda):
+	# create a train dataset
+	train_coco = COCO(os.path.join(args.train_annotations_small_path))
+	# create a val dataset
+	val_coco = COCO(os.path.join(args.val_annotations_small_path))
+
+	train_ds = TrainImageDataset(img_dir = args.train_image_directory , cocodataset = train_coco, y_data = None, 
+	                             input_img_resize = args.input_img_resize,output_img_resize = args.output_img_resize, X_transform=trans_aug.augment_img)
+
+	train_loader = DataLoader(train_ds, args.batch_size,sampler=RandomSampler(train_ds), num_workers=threads,pin_memory=use_cuda)
+
+	valid_ds = TrainImageDataset(img_dir = args.val_image_directory, cocodataset = val_coco, y_data = None, 
+	                             input_img_resize = args.input_img_resize, output_img_resize = args.output_img_resize, X_transform=trans_aug.augment_img)
+
+	valid_loader = DataLoader(valid_ds, args.batch_size,
+	                          sampler=SequentialSampler(valid_ds),
+	                          num_workers=threads,
+	                          pin_memory=use_cuda)
+	# test dataset
+
+	
+	test_ds = TestImageDataset(img_dir = args.test_image_directory, img_resize = args.input_img_resize)
+	test_loader = DataLoader(test_ds, args.batch_size,
+	                         sampler=SequentialSampler(test_ds),
+	                         num_workers=threads,
+	                         pin_memory=use_cuda)
+
+
+	return train_ds, valid_ds, test_ds
+
+def main():
+	parser = argparse.ArgumentParser()
+    parser.add_argument('project_dir', type=str, default='/home/webwerks/patricia/my-projects/github/Segmentation', help='project directory')
+    os.chdir(project_dir)
+    parser.add_argument('train_image_directory', type=str, default = "data/train/images")
+    parser.add_argument('train_annotations_path', type=str, default = "data/train/annotation.json")
+    parser.add_argument('train_annotations_small_path', type=str, default = "data/train/annotation-small.json")
+    parser.add_argument('val_image_directory', type=str, default = "data/val/images")
+    parser.add_argument('val_annotations_path', type=str, default = "data/val/annotation.json")
+    parser.add_argument('val_annotations_small_path', type=str, default = "data/val/annotation-small.json")
+    parser.add_argument('test_image_directory', type=str, default = "data/test")
+
+    parser.add_argument('input_img_resize', type=tuple, default = (300, 300), help='The resize size of the input images of the neural net')
+    parser.add_argument('output_img_resize', type=tuple, default = (300, 300), help='The resize size of the output images of the neural net')
+    parser.add_argument('batch_size', type=int, default = 3)
+	parser.add_argument('epochs', type=int, default = 50)
+	parser.add_argument('threshold', type=float, default = 0.5)
+	parser.add_argument('validation_size', type=float, default = 0.2)\
+	# Put 'None' to work on full dataset or a value between 0 and 1
+	parser.add_argument('sample_size', type=float, default = None)
+
+    
+    args = parser.parse_args()
+    
+
+	# -- Optional parameters
+	threads = cpu_count()
+	use_cuda = torch.cuda.is_available()
+
+	train_ds, valid_ds, test_ds = create_train_val_test_dataloaders(args, threads, use_cuda)
+	tb_viz_cb, tb_logs_cb, model_saver_cb, pred_saver_cb = create_call_backs(args)
+	
+
+
+
